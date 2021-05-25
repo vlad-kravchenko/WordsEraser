71aa3920bfeb27495f4a33a71fc4dd972c5cd927
Vladimir Kravchenko <56361236+vlad-kravchenko@users.noreply.github.com>
2021-05-25 20:19:13 +0300

    Initial

---
+++ WordsEraser/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WordsEraser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.tbSource = new System.Windows.Forms.RichTextBox();
+            this.tbResult = new System.Windows.Forms.RichTextBox();
+            this.tbFrequency = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(118, 12);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(854, 23);
+            this.btnReplace.TabIndex = 0;
+            this.btnReplace.Text = "Replace words";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(12, 41);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(960, 327);
+            this.tbSource.TabIndex = 1;
+            this.tbSource.Text = "";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(12, 374);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(960, 275);
+            this.tbResult.TabIndex = 2;
+            this.tbResult.Text = "";
+            // 
+            // tbFrequency
+            // 
+            this.tbFrequency.Location = new System.Drawing.Point(12, 12);
+            this.tbFrequency.Name = "tbFrequency";
+            this.tbFrequency.Size = new System.Drawing.Size(100, 20);
+            this.tbFrequency.TabIndex = 3;
+            this.tbFrequency.Text = "90";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.tbFrequency);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.btnReplace);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "WordsEraser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.RichTextBox tbSource;
+        private System.Windows.Forms.RichTextBox tbResult;
+        private System.Windows.Forms.TextBox tbFrequency;
+    }
+}
+
---
+++ WordsEraser/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WordsEraser
+{
+    public partial class MainForm : Form
+    {
+        private static readonly Random Rand = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string text = tbSource.Text;
+            string result = string.Empty;
+            var words = text.Split(' ');
+            foreach (var word in words)
+            {
+                int flag = Rand.Next(100);
+                if (flag < double.Parse(tbFrequency.Text))
+                {
+                    foreach (var letter in word)
+                    {
+                        result += letter;
+                    }
+                }
+                else
+                {
+                    foreach (var letter in word)
+                    {
+                        if (char.IsLetter(letter))
+                        {
+                            result += "*";
+                        }
+                        else
+                        {
+                            result += letter;
+                        }
+                    }
+                }
+                result += " ";
+            }
+
+            tbResult.Text = result;
+        }
+    }
+}
